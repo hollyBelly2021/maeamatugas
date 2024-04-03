8227ed0f9a6393f2a6f613a585f5ffcee3c7cf0e
Maea Matugas <matugasmaea@gmail.com>
2024-04-03 16:41:23 -0500

    navigation update

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("JavaScript is ready to run");
+// const windowWidth = window.innerWidth;
+let navChecker = false;
+function openNav() {
+  navChecker = true;
+  document.querySelector("body").style.marginLeft = "25rem";
+}
+
+function closeNav() {
+  navChecker = false;
+  document.querySelector("body").style.marginLeft = "0";
+}
+
+window.addEventListener("resize", function () {
+  //   document.querySelector("body").style.marginLeft = "0";
+  let windowWidth = window.innerWidth;
+  if (windowWidth > 772) {
+    document.querySelector("body").style.marginLeft = "0";
+  } else if (windowWidth < 772 && navChecker) {
+    document.querySelector("body").style.marginLeft = "25rem";
+  }
+});
+
+// if (windowWidth > 772) {
+//   document.querySelector("body").style.marginLeft = "0";
+// }
+
+// function closeNav() {
+//   //   document.querySelector(".navbar").style.width = "0";
+
+//   document.querySelector("body").style.marginLeft = "0";
+// }
